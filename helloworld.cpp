dc611afac366fb82bed09b1087a17d326d6bcac6
Priti <pritibhardwaj118@gmail.com>
2024-08-05 22:59:00 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+//Priti
+//23070123103
+//printing hello world 
+#include<iostream>
+using namespace std;//standarad namespace in c++ library where classes objects and all other are declared
+int main()//predefined function used in c++
+{
+  cout<<"Hello world"; // printing hello world 
+  return 0;  
+}
---
+++ input_from_user.cpp
@@ -0,0 +1,13 @@
+//Priti
+//23070123103
+//exp 1- getting input from user 
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num; // declaing a variable num as integer type
+   cout<<"Enter a number: ";
+   cin>>num;//taking the input from user in num 
+   cout<<"The number inputed by you is: "<<num;//printing the number 
+   return 0;
+}
